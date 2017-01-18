666a0e9d7597d1c98327a4e62d845aa78f7d6a14
Luis Alberto Hernández Ramos <lhernandez@OCCQRO.local>
2017-01-18 12:13:36 -0600

    testing the component countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,12 +4,32 @@ const CountdownForm = require('CountdownForm');
 
 const Countdown = React.createClass({
 	getInitialState: function(){
-		return {count:0};
+		return {
+			count:0,
+			countDownStatus: 'stopped'
+		};
 	},
-
+	componentDidUpdate: function(prevProps, prevState) {
+		if(this.state.countDownStatus !== prevState.countDownStatus){
+			switch(this.state.countDownStatus){
+				case 'started':
+					this.startTimer();
+					break;
+			}
+		}
+	},
+	startTimer: function(){
+		this.timer = setInterval(() => {
+			var newCount = this.state.count - 1;
+			this.setState({
+				count: newCount >=0 ? newCount : 0
+			});
+		}, 1000);
+	},
 	handleSetCountdown: function(seconds){
 		this.setState({
-			count:seconds
+			count:seconds,
+			countDownStatus: 'started'
 		});
 	},
 	render: function(){
---
+++ app/test/Countdown.test.jsx
@@ -0,0 +1,45 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+
+	it('Should exist', () => {
+		expect(Countdown).toExist();
+	});
+});
+
+describe('handleSetCountdown', () => {
+
+	it('Should set state to started and countdown', () => {
+		var countdown = TestUtils.renderIntoDocument(<Countdown />);
+		countdown.handleSetCountdown(10);
+
+		expect(countdown.state.count).toBe(10);
+		expect(countdown.state.countDownStatus).toBe('started');
+
+		setTimeout(() => {
+			expect(countdown.state.count).toBe(9);
+		}, 1001);
+	});
+
+	it('Should never set count less than zero', () => {
+		var countdown = TestUtils.renderIntoDocument(<Countdown />);
+		countdown.handleSetCountdown(1);
+
+		setTimeout(() => {
+			expect(countdown.state.count).toBe(0);
+		}, 3000);
+	});
+});
+
+
+
+
+// describre('', ()=>{
+// 	it('', () => {});
+// });
