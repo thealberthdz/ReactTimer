0def8480eec673af3e12f5abdbc2332827c302a5
Luis Alberto Hernández Ramos <lhernandez@OCCQRO.local>
2017-01-17 18:45:17 -0600

    Creating a circle and testing Clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,12 @@
 const React = require('react');
+const Clock = require('Clock');
 
 const Countdown = React.createClass({
 	render: function(){
 		return(
 			<div>
-				<h2>Countdown components</h2>
+				<h2>Countdown</h2>
+				<Clock totalSeconds = {200} />
 			</div>
 		);
 	}
